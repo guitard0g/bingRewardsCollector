d8636b6dc7da39c2f4d26eda173f3675289216c6
jjrylearn <joshuaryanlearn@gmail.com>
2016-10-18 03:29:19 -0400

    the rewards collecting script

---
+++ rewardCollector.py
@@ -0,0 +1,39 @@
+
+
+#the emails associated with all of your microsoft accounts
+emails = ['email1@example1.com', 'email2@example2.com']
+
+from time import sleep
+
+#will need to install the package splinter for python 3 for this to work
+from splinter import Browser
+
+for email in emails:
+    with Browser('chrome') as b:
+        # stuff using the b
+        # b = Browser('chrome')
+        b.visit('https://login.live.com')
+        # b.find_by_text('Email or phone')
+        b.fill("loginfmt", email)
+        #change myPassword to your actual password
+        b.fill("passwd", "myPassword")
+        b.find_by_id('idSIButton9').click()
+        #bunch of random things that the script will search to collect your rewards, these are arbitrary and can be anything really
+        arr = ["matlab", "things", "nike shoes", "kickstarter", "android development", "apple iphone", "mountain range", "appalachian plateau", "how to implement a map in python", "cython documentation", "champion.gg league", "columbia computer science", "how to make a paper airplane", "edx courses", "computer science college rankings", "longest increasing subsequence algorithm", "yardwork", "skyrim", "no man's sky", "undertale", "ocaml", "jane street",  'reddit', 'query', 'string', 'imgur', 'array', ' python list',
+     'words', 'excluded definition','Teach me how to twerk', 'vine videos', 'deep ocean', 'giraffes', 'atlantis']
+        print(len(arr))
+        for i in range(len(arr)):
+    	    sleep(5)
+    	    b.visit('https://www.bing.com')
+    	    b.fill("q", arr[i])
+    	    b.find_by_id('sb_form_go').click()
+
+
+
+
+
+
+
+
+
+
