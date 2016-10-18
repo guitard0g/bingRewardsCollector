e6950e75fe36d81af59f96f24dc70a3b8c179c48
Josh <joshuaryanlearn@gmail.com>
2016-10-18 03:45:54 -0400

    readme edits and slight change to the script

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # bingRewardsCollector
 Scripts to automatically collect bing rewards for you
+
+In order to run this script you will need python 3 and the splinter package for python 3 installed.
+
+You can use your computers task scheduler to run this file at a specified time each day so that you never miss out on rewards :)
+
+All you need to do is change the emails and password to that which you use for your microsoft account 
+
+To run this script you simply need to type "python rewardsCollector.py" in your command prompt.
--- rewardCollector.py
+++ rewardCollector.py
@@ -1,7 +1,7 @@
 
 
-#the emails associated with all of your microsoft accounts
-emails = ['email1@example1.com', 'email2@example2.com']
+#the emails associated with all of your microsoft accounts and their respective passwords
+emails = [('email1@example1.com', 'password1'), ('email2@example2.com', 'password2')]
 
 from time import sleep
 
@@ -14,9 +14,8 @@ for email in emails:
         # b = Browser('chrome')
         b.visit('https://login.live.com')
         # b.find_by_text('Email or phone')
-        b.fill("loginfmt", email)
-        #change myPassword to your actual password
-        b.fill("passwd", "myPassword")
+        b.fill("loginfmt", email[0])
+        b.fill("passwd", email[1])
         b.find_by_id('idSIButton9').click()
         #bunch of random things that the script will search to collect your rewards, these are arbitrary and can be anything really
         arr = ["matlab", "things", "nike shoes", "kickstarter", "android development", "apple iphone", "mountain range", "appalachian plateau", "how to implement a map in python", "cython documentation", "champion.gg league", "columbia computer science", "how to make a paper airplane", "edx courses", "computer science college rankings", "longest increasing subsequence algorithm", "yardwork", "skyrim", "no man's sky", "undertale", "ocaml", "jane street",  'reddit', 'query', 'string', 'imgur', 'array', ' python list',
